d139e79c12284edc0ad45681aa0b02a65b22a11d
AkoU07 <69804971+AkoU07@users.noreply.github.com>
2020-11-27 23:37:09 +0100

    camera script

---
+++ Assets/Camera_lab/FollowCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    public GameObject Pointer;
+    private float maxY = -5.5f;
+    private float miniY = -24.6f;
+    private float nextY = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Calcule de la position Y avec hauteur Max et Mini
+        if (Pointer.transform.position.y > maxY)
+        {
+            nextY = maxY;
+        }
+        else if (Pointer.transform.position.y < miniY)
+        {
+            nextY = miniY;
+        }
+        else {
+            nextY = Pointer.transform.position.y;
+        }
+        Debug.Log("nextY: " + nextY + " - Y pointer: " + Pointer.transform.position.y);
+        transform.position = new Vector3(transform.position.x, nextY, transform.position.z);
+    }
+}
